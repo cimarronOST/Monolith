12e26d8257bc0f7efed4c8fc99bd8ae868ca9308
cimarronOST <cimarronOST@gmail.com>
2017-05-25 23:26:20 +0200

    Update convert.cpp

--- Source/convert.cpp
+++ Source/convert.cpp
@@ -1,5 +1,5 @@
 /*
-  Monolith 0.1  Copyright (C) 2017 Jonas Mayr
+  Monolith 0.2  Copyright (C) 2017 Jonas Mayr
 
   This file is part of Monolith.
 
@@ -25,16 +25,13 @@
 namespace
 {
 	const uint8 piece_char[]{ 'R', 'N', 'B', 'Q', 'K' };
-
-	const uint64 file_h{ 0x101010101010101 };
-	const uint64 rank_1{ 0xff };
 }
 
-uint64 conv::to_bb(string sq)
+uint64 conv::to_bb(const string sq)
 {
 	return 1ULL << to_int(sq);
 }
-int conv::to_int(string sq)
+int conv::to_int(const string sq)
 {
 	assert(sq.size() == 2U);
 	assert( isdigit(unsigned(sq.back())));
@@ -49,12 +46,12 @@ int conv::to_int(string sq)
 	return shift;
 }
 
-string conv::to_str(uint64 &sq)
+string conv::to_str(const uint64 &sq)
 {
 	bb::bitscan(sq);
 	return to_str(static_cast<int>(bb::lsb()));
 }
-string conv::to_str(int sq)
+string conv::to_str(const int sq)
 {
 	string str;
 	str += 'h' - static_cast<char>(sq & 7);
@@ -68,22 +65,25 @@ uint16 conv::san_to_move(pos &board, string move)
 	uint8 flag{ 0 };
 	uint16 sq1{ 0 }, sq2{ 0 };
 
-	//// castling short
+	// castling short
+
 	if (move == "O-O")
 	{
-		const uint16 squares[]{ e1, g1, e8, g8 };
+		const uint16 sqs[]{ E1, G1, E8, G8 };
 
-		sq1 = squares[board.turn * 2];
-		sq2 = squares[board.turn * 2 + 1];
+		sq1 = sqs[board.turn * 2];
+		sq2 = sqs[board.turn * 2 + 1];
 		flag = castl_e::SHORT_WHITE + board.turn * 2;
 	}
-	//// castling long
+
+	// castling long
+
 	else if (move == "O-O-O")
 	{
-		const uint16 squares[]{ e1, c1, e8, c8 };
+		const uint16 sqs[]{ E1, C1, E8, C8 };
 
-		sq1 = squares[board.turn * 2];
-		sq2 = squares[board.turn * 2 + 1];
+		sq1 = sqs[board.turn * 2];
+		sq2 = sqs[board.turn * 2 + 1];
 		flag = castl_e::LONG_WHITE + board.turn * 2;
 	}
 	else
@@ -91,7 +91,8 @@ uint16 conv::san_to_move(pos &board, string move)
 		if (move.back() == '+')
 			move.pop_back();
 
-		//// promotion
+		// promotion
+
 		char promo{ move.back() };
 
 		for (int i{ 0 }; i < 4; ++i)
@@ -102,7 +103,8 @@ uint16 conv::san_to_move(pos &board, string move)
 				break;
 			}
 
-		//// creating square 1
+		// creating square 1
+
 		if (move.size() >= 2
 			&&  isdigit(static_cast<unsigned>(move.back()))
 			&& !isdigit(static_cast<unsigned>(move[move.size() - 2])))
@@ -111,7 +113,8 @@ uint16 conv::san_to_move(pos &board, string move)
 			move.erase(move.size() - 2, move.size());
 		}
 
-		//// in case of a pawn, piece = file of the pawn
+		// in case of a pawn, piece = file of the pawn
+
 		char piece{ 0 };
 		if (!move.empty())
 		{
@@ -119,36 +122,41 @@ uint16 conv::san_to_move(pos &board, string move)
 			move.erase(0, 1);
 		}
 
-		//// square specifies the file or rank of the piece
+		// square specifies the file or rank of the piece
+
 		char square{ 0 };
 		if (move != "x" && !move.empty())
 			square = move.front();
 
 		movegen gen(board, ALL);
 
-		//// creating square 1
+		// creating square 1
+
 		int possible[8]{ 0 };
 		int count{ 0 };
 		for (int idx{ 0 }; idx < gen.move_cnt; ++idx)
 		{
-			if (to_sq2(gen.movelist[idx]) == sq2)
+			if (to_sq2(gen.list[idx]) == sq2)
 				possible[count++] = idx;
 		}
 
-		//// no legal move found
+		// no legal move found
+
 		if (count == 0)
 			return 0;
 
-		//// one possible move
+		// one possible move
 		else if (count == 1)
-			sq1 = to_sq1(gen.movelist[possible[0]]);
+			sq1 = to_sq1(gen.list[possible[0]]);
 
-		//// more possible moves
+		// more possible moves
+
 		else
 		{
 			uint64 occ{ 0 };
 
-			//// piece move
+			// piece move
+
 			for (int p{ ROOKS }; p <= KINGS; ++p)
 				if (piece_char[p-1] == piece)
 				{
@@ -156,32 +164,35 @@ uint16 conv::san_to_move(pos &board, string move)
 					break;
 				}
 
-			//// pawn move
+			// pawn move
+
 			if (!occ && piece)
 			{
 				occ = board.pieces[PAWNS];
-				occ &= file_h << ('h' - piece);
+				occ &= file[H] << ('h' - piece);
 			}
 
-			//// piece rank/file
+			// piece rank/file
+
 			if (square != 0)
 			{
 				int sq{ square - '1' };
 				if (sq >= 0 && sq < 8)
-					occ &= rank_1 << (sq * 8);
+					occ &= rank[R1] << (sq * 8);
 				else
 				{
 					sq = 'h' - square;
 					assert(sq >= 0 && sq < 8);
-					occ &= file_h << sq;
+					occ &= file[H] << sq;
 				}
 			}
 
-			//// finding the right move
+			// finding the right move
+
 			for (int idx{ 0 }; idx < count; ++idx)
-				if ((1ULL << to_sq1(gen.movelist[possible[idx]])) & occ)
+				if ((1ULL << to_sq1(gen.list[possible[idx]])) & occ)
 				{
-					sq1 = to_sq1(gen.movelist[possible[idx]]);
+					sq1 = to_sq1(gen.list[possible[idx]]);
 					break;
 				}
 
@@ -195,9 +206,9 @@ uint16 conv::san_to_move(pos &board, string move)
 
 	return encode(sq1, sq2, flag);
 }
-string conv::bit_to_san(pos &board, uint64 &sq1_64, uint64 &sq2_64, uint8 flag)
+string conv::bit_to_san(pos &board, const uint64 &sq1_64, const uint64 &sq2_64, uint8 flag)
 {
-	//// works only if engine::new_move() has not yet been called
+	// works only if engine::new_move() has not yet been called
 
 	string move;
 	movegen gen(board, ALL);
@@ -208,16 +219,19 @@ string conv::bit_to_san(pos &board, uint64 &sq1_64, uint64 &sq2_64, uint8 flag)
 	bb::bitscan(sq2_64);
 	int sq2{ static_cast<int>(bb::lsb()) };
 
-	//// castling short
+	// castling short
+
 	if (flag == castl_e::SHORT_WHITE || flag == castl_e::SHORT_BLACK)
 	{
-		assert((sq1_64 & 0x800000000000008) && (sq2_64 & 0x200000000000002));
+		assert((sq1 == E1 && sq2 == G1) || (sq1 == E8 && sq2 == G8));
 		move = "O-O";
 	}
-	//// castling long
+
+	// castling long
+
 	else if (flag == castl_e::LONG_WHITE || flag == castl_e::LONG_BLACK)
 	{
-		assert((sq1_64 & 0x800000000000008) && (sq2_64 & 0x2000000000000020));
+		assert((sq1 == E1 && sq2 == C1) || (sq1 == E8 && sq2 == C8));
 		move = "O-O-O";
 	}
 	else
@@ -226,7 +240,8 @@ string conv::bit_to_san(pos &board, uint64 &sq1_64, uint64 &sq2_64, uint8 flag)
 		string sq1_str{ to_str(sq1_64) };
 		auto pc{ board.piece_sq[sq1] };
 
-		//// adding piece or pawn-file
+		// adding piece or pawn-file
+
 		if (pc != PAWNS)
 		{
 			move += piece_char[pc - 1];
@@ -238,41 +253,46 @@ string conv::bit_to_san(pos &board, uint64 &sq1_64, uint64 &sq2_64, uint8 flag)
 			move = sq1_str.front();
 		}
 
-		//// collecting all legal moves to square 2
+		// collecting all legal moves to square 2
+
 		uint64 possible{ 0 };
 		for (int cnt{ 0 }; cnt < gen.move_cnt; ++cnt)
 		{
-			if (1ULL << to_sq2(gen.movelist[cnt]) == sq2_64 && piece & (1ULL << to_sq1(gen.movelist[cnt])))
-				possible |= 1ULL << to_sq1(gen.movelist[cnt]);
+			if (1ULL << to_sq2(gen.list[cnt]) == sq2_64 && piece & (1ULL << to_sq1(gen.list[cnt])))
+				possible |= 1ULL << to_sq1(gen.list[cnt]);
 		}
 
-		//// specifying which move is the right one
+		// specifying which move is the right one
+
 		if (possible & (possible - 1))
 		{
-			auto rank{ sq1_str.back() - '1' };
-			auto file{ 'h' - sq1_str.front() };
+			auto rank_idx{ sq1_str.back() - '1' };
+			auto file_idx{ 'h' - sq1_str.front() };
 
-			assert(1ULL << (file + 8 * rank) == sq1_64);
+			assert(1ULL << (file_idx + 8 * rank_idx) == sq1_64);
 
-			if (bb::popcnt(possible & (rank_1 << rank)) >= 2)
+			if (bb::popcnt(possible & rank[rank_idx]) >= 2)
 				move += sq1_str.front();
-			else if (bb::popcnt(possible & (file_h << file)) >= 2)
+			else if (bb::popcnt(possible & file[file_idx]) >= 2)
 				move += sq1_str.back();
 			else
 				move += sq1_str.front();
 		}
 
-		//// adding square 2
+		// adding square 2
+
 		if (sq2_64 & board.side[BOTH])
 			move += "x";
 		move += to_str(sq2_64);
 
-		//// adding promo
+		// adding promo
+
 		if(to_promo(flag) != " ")
 			move += to_promo(flag).front();
 	}
 
-	//// execute the move to see if there is a check
+	// execute the move to see if there is a check
+
 	uint16 move_temp{ encode(sq1, sq2, flag) };
 
 	pos save(board);
@@ -280,8 +300,8 @@ string conv::bit_to_san(pos &board, uint64 &sq1_64, uint64 &sq2_64, uint8 flag)
 
 	board.new_move(move_temp);
 
-	uint64 king_enemy{ board.side[board.turn ^ 1] & board.pieces[KINGS] };
-	if (!gen.check(board, board.turn ^ 1, king_enemy))
+	uint64 enemy_king{ board.side[board.turn ^ 1] & board.pieces[KINGS] };
+	if (!gen.check(board, board.turn ^ 1, enemy_king))
 		move += "+";
 
 	board = save;
@@ -308,7 +328,7 @@ string conv::to_promo(uint8 flag)
 	}
 	return promo;
 }
-uint8 conv::to_flag(char promo, pos &board, uint64 &sq1_64, uint64 &sq2_64)
+uint8 conv::to_flag(char promo, const pos &board, const uint64 &sq1_64, const uint64 &sq2_64)
 {
 	assert(sq1_64 != 0 && sq2_64 != 0);
 
@@ -317,30 +337,34 @@ uint8 conv::to_flag(char promo, pos &board, uint64 &sq1_64, uint64 &sq2_64)
 	bb::bitscan(sq2_64);
 	int sq2{ static_cast<int>(bb::lsb()) };
 
-	//// captured piece
+	// captured piece
+
 	auto flag{ board.piece_sq[sq2] };
 	auto piece{ board.piece_sq[sq1] };
 
 	if (piece == PAWNS)
 	{
-		//// enpassant
+		// enpassant
+
 		if ((~board.side[BOTH] & sq2_64) && abs(sq1 - sq2) % 8 != 0)
 			flag = ENPASSANT;
 
-		//// promotion
+		// promotion
+
 		if (promo == 'q') flag = PROMO_QUEEN;
 		else if (promo == 'r') flag = PROMO_ROOK;
 		else if (promo == 'n') flag = PROMO_KNIGHT;
 		else if (promo == 'b') flag = PROMO_BISHOP;
 	}
 
-	//// castling
+	// castling
+
 	else if (piece == KINGS)
 	{
-		if (sq1 == e1 && sq2 == g1) flag = castl_e::SHORT_WHITE;
-		else if (sq1 == e1 && sq2 == c1) flag = castl_e::LONG_WHITE;
-		else if (sq1 == e8 && sq2 == g8) flag = castl_e::SHORT_BLACK;
-		else if (sq1 == e8 && sq2 == c8) flag = castl_e::LONG_BLACK;
+		if (sq1 == E1 && sq2 == G1) flag = castl_e::SHORT_WHITE;
+		else if (sq1 == E1 && sq2 == C1) flag = castl_e::LONG_WHITE;
+		else if (sq1 == E8 && sq2 == G8) flag = castl_e::SHORT_BLACK;
+		else if (sq1 == E8 && sq2 == C8) flag = castl_e::LONG_BLACK;
 	}
 	else
 		assert(piece > PAWNS && piece < KINGS);
