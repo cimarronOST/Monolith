4fcf304fe2c16a1316252dbc3e014544fa2be945
cimarronOST <cimarronOST@gmail.com>
2017-05-25 23:34:17 +0200

    Update position.cpp

--- Source/position.cpp
+++ Source/position.cpp
@@ -1,5 +1,5 @@
 /*
-  Monolith 0.1  Copyright (C) 2017 Jonas Mayr
+  Monolith 0.2  Copyright (C) 2017 Jonas Mayr
 
   This file is part of Monolith.
 
@@ -62,17 +62,17 @@ void pos::new_move(uint16 move)
 	auto piece{ piece_sq[sq1] };
 	auto victim{ piece_sq[sq2] };
 
-	int piece12;
-
 	assert(flag == victim || flag >= 7);
 	assert(piece != NONE);
 
-	//// preventing castling
-	auto saved_castl_r{ castl_rights };
-	rook_moved(sq2_64);
-	rook_moved(sq1_64);
+	// preventing castling
 
-	//// deleting the eventually captured piece
+	auto saved_castl_r{ castl_rights };
+	rook_moved(sq2_64, sq2);
+	rook_moved(sq1_64, sq1);
+
+	// deleting the eventually captured piece
+
 	if (victim != NONE)
 	{
 		assert(sq2_64 & side[turn ^ 1]);
@@ -80,10 +80,9 @@ void pos::new_move(uint16 move)
 
 		side[turn ^ 1] &= ~sq2_64;
 		pieces[victim] &= ~sq2_64;
-		phase -= phase_value[victim];
 
-		piece12 = hashing::piece_12[victim];
-		key ^= hashing::random64[(piece12 - turn) * 64 +  mirror(sq2)];
+		phase -= phase_value[victim];
+		key ^= hashing::random64[(hashing::piece_12[victim] - turn) * 64 + mirror(sq2)];
 
 		assert(phase >= 0);
 	}
@@ -110,30 +109,33 @@ void pos::new_move(uint16 move)
 		}
 	}
 
-	//// pawn double push
+	// pawn double push
+
 	ep_square = 0ULL;
 	if (piece == PAWNS && abs(sq1 - sq2) == 16)
 	{
 		ep_square = shift(sq1_64, push[turn]);
 
-		auto file{ sq1_64 & 7 };
-		if (pieces[PAWNS] & side[turn ^ 1] & hashing::ep_flank[turn][file])
-			key ^= hashing::random64[hashing::offset.ep + 7 - file];
+		auto file_idx{ sq1 & 7 };
+		if (pieces[PAWNS] & side[turn ^ 1] & hashing::ep_flank[turn][file_idx])
+			key ^= hashing::random64[hashing::offset.ep + 7 - file_idx];
 	}
 
-	//// doing the move
+	// doing the move
+
 	pieces[piece] ^= sq1_64; side[turn] ^= sq1_64;
 	pieces[piece] |= sq2_64; side[turn] |= sq2_64;
 	piece_sq[sq2] = piece;
 	piece_sq[sq1] = NONE;
 
-	piece12 = hashing::piece_12[piece];
+	int piece12 = hashing::piece_12[piece];
 	key ^= hashing::random64[(piece12 - turn) * 64 + mirror(sq1)];
 	key ^= hashing::random64[(piece12 - turn) * 64 + mirror(sq2)];
 
 	if (flag >= 8)
 	{
-		//// castling
+		// castling
+
 		if (flag <= 11)
 		{
 			switch (flag)
@@ -141,7 +143,7 @@ void pos::new_move(uint16 move)
 			case castl_e::SHORT_WHITE:
 				pieces[ROOKS] ^= 0x1, side[turn] ^= 0x1;
 				pieces[ROOKS] |= 0x4, side[turn] |= 0x4;
-				piece_sq[h1] = NONE, piece_sq[f1] = ROOKS;
+				piece_sq[H1] = NONE, piece_sq[F1] = ROOKS;
 				
 				key ^= hashing::random64[(7 - turn) * 64 + 7];
 				key ^= hashing::random64[(7 - turn) * 64 + 5];
@@ -150,7 +152,7 @@ void pos::new_move(uint16 move)
 			case castl_e::SHORT_BLACK:
 				pieces[ROOKS] ^= 0x100000000000000, side[turn] ^= 0x100000000000000;
 				pieces[ROOKS] |= 0x400000000000000, side[turn] |= 0x400000000000000;
-				piece_sq[h8] = NONE, piece_sq[f8] = ROOKS;
+				piece_sq[H8] = NONE, piece_sq[F8] = ROOKS;
 
 				key ^= hashing::random64[(7 - turn) * 64 + 63];
 				key ^= hashing::random64[(7 - turn) * 64 + 61];
@@ -159,7 +161,7 @@ void pos::new_move(uint16 move)
 			case castl_e::LONG_WHITE:
 				pieces[ROOKS] ^= 0x80, side[turn] ^= 0x80;
 				pieces[ROOKS] |= 0x10, side[turn] |= 0x10;
-				piece_sq[a1] = NONE, piece_sq[d1] = ROOKS;
+				piece_sq[A1] = NONE, piece_sq[D1] = ROOKS;
 
 				key ^= hashing::random64[(7 - turn) * 64];
 				key ^= hashing::random64[(7 - turn) * 64 + 3];
@@ -168,7 +170,7 @@ void pos::new_move(uint16 move)
 			case castl_e::LONG_BLACK:
 				pieces[ROOKS] ^= 0x8000000000000000, side[turn] ^= 0x8000000000000000;
 				pieces[ROOKS] |= 0x1000000000000000, side[turn] |= 0x1000000000000000;
-				piece_sq[a8] = NONE, piece_sq[d8] = ROOKS;
+				piece_sq[A8] = NONE, piece_sq[D8] = ROOKS;
 
 				key ^= hashing::random64[(7 - turn) * 64 + 56];
 				key ^= hashing::random64[(7 - turn) * 64 + 59];
@@ -179,7 +181,8 @@ void pos::new_move(uint16 move)
 			}
 		}
 
-		//// promotion
+		// promotion
+
 		else
 		{
 			int promo_p{ flag - 11 };
@@ -192,25 +195,30 @@ void pos::new_move(uint16 move)
 			pieces[promo_p] |= sq2_64;
 			piece_sq[sq2] = promo_p;
 
-			piece12 = hashing::piece_12[promo_p];
 			int sq_new{ mirror(sq2) };
 			key ^= hashing::random64[(BLACK - turn) * 64 + sq_new];
-			key ^= hashing::random64[(piece12 - turn) * 64 + sq_new];
+			key ^= hashing::random64[(hashing::piece_12[promo_p] - turn) * 64 + sq_new];
 
 			phase += phase_value[promo_p];
 		}
 	}
 
-	//// preventing castling when king is moved
+	// preventing castling when king is moved
+
 	if (sq2_64 & pieces[KINGS])
+	{
 		castl_rights &= castl_r[turn];
+		king_sq[turn] = sq2;
+	}
 
 	if (saved_castl_r != castl_rights)
 	{
 		auto changes{ saved_castl_r ^ castl_rights };
 		for (int i{ 0 }; i < 4; ++i)
+		{
 			if (changes & castl_right[i])
 				key ^= hashing::random64[hashing::offset.castl + i];
+		}
 	}
 
 	turn ^= 1;
@@ -222,32 +230,35 @@ void pos::new_move(uint16 move)
 
 void pos::null_move(uint64 &ep_copy)
 {
-	//// updating hash key
+	// updating hash key
+
 	key ^= hashing::is_turn[0];
 	if (ep_square != 0)
 	{
 		bb::bitscan(ep_square);
-		auto file{ bb::lsb() & 7 };
-		if (pieces[PAWNS] & side[turn ^ 1] & hashing::ep_flank[turn][file])
-			key ^= hashing::random64[hashing::offset.ep + 7 - file];
+		auto file_idx{ bb::lsb() & 7 };
+		if (pieces[PAWNS] & side[turn ^ 1] & hashing::ep_flank[turn][file_idx])
+			key ^= hashing::random64[hashing::offset.ep + 7 - file_idx];
 	}
 
 	ep_copy = ep_square;
 	ep_square = 0;
 	half_moves += 1;
 	moves += 1;
+	nodes += 1;
 	turn ^= 1;
 }
 void pos::undo_null_move(uint64 &ep_copy)
 {
-	//// updating hash key
+	// updating hash key
+
 	key ^= hashing::is_turn[0];
 	if (ep_copy != 0)
 	{
 		bb::bitscan(ep_copy);
-		auto file{ bb::lsb() & 7 };
-		if (pieces[PAWNS] & side[turn ^ 1] & hashing::ep_flank[turn][file])
-			key ^= hashing::random64[hashing::offset.ep + 7 - file];
+		auto file_idx{ bb::lsb() & 7 };
+		if (pieces[PAWNS] & side[turn ^ 1] & hashing::ep_flank[turn][file_idx])
+			key ^= hashing::random64[hashing::offset.ep + 7 - file_idx];
 	}
 
 	ep_square = ep_copy;
@@ -264,8 +275,8 @@ void pos::clear()
 	moves = 0;
 	half_moves = 0;
 	castl_rights = 0;
-	ep_square = 0;
-	turn = white;
+	ep_square = 0ULL;
+	turn = WHITE;
 	phase = 0;
 }
 void pos::parse_fen(string fen)
@@ -274,7 +285,7 @@ void pos::parse_fen(string fen)
 	int sq{ 63 };
 	uint32 focus{ 0 };
 	assert(focus < fen.size());
-	
+
 	while (focus < fen.size() && fen[focus] != ' ')
 	{
 		assert(sq >= 0);
@@ -293,18 +304,20 @@ void pos::parse_fen(string fen)
 		else
 		{
 			for (int piece{ PAWNS }; piece <= KINGS; ++piece)
-				for (int color{ WHITE }; color <= BLACK; ++color)
+			{
+				for (int col{ WHITE }; col <= BLACK; ++col)
 				{
-					if (fen[focus] == p_char[color][piece])
+					if (fen[focus] == p_char[col][piece])
 					{
 						pieces[piece] |= 1ULL << sq;
-						side[color] |= 1ULL << sq;
+						side[col] |= 1ULL << sq;
 						piece_sq[sq] = piece;
 
 						phase += phase_value[piece];
 						break;
 					}
 				}
+			}
 			sq -= 1;
 		}
 		focus += 1;
@@ -313,12 +326,24 @@ void pos::parse_fen(string fen)
 
 	side[BOTH] = side[WHITE] | side[BLACK];
 	assert(side[BOTH] == (side[WHITE] ^ side[BLACK]));
+
+	// king squares
+
+	for (int col{ WHITE }; col <= BLACK; ++col)
+	{
+		bb::bitscan(pieces[KINGS] & side[col]);
+		king_sq[col] = bb::lsb();
+	}
 	
+	// turn
+
 	focus += 1;
 	if (fen[focus] == 'w')
-		turn = white;
+		turn = WHITE;
 	else if (fen[focus] == 'b')
-		turn = black;
+		turn = BLACK;
+	
+	// castling
 
 	focus += 2;
 	while (focus < fen.size() && fen[focus] != ' ')
@@ -336,6 +361,8 @@ void pos::parse_fen(string fen)
 		focus += 1;
 	}
 
+	// enpassant
+
 	focus += 1;
 	if (fen[focus] == '-')
 		focus += 1;
@@ -348,11 +375,15 @@ void pos::parse_fen(string fen)
 	if (focus >= fen.size() - 1)
 		return;
 
+	// half move count
+
 	focus += 1;
 	string half_move_c;
 	while (focus < fen.size() && fen[focus] != ' ')
 		half_move_c += fen[focus++];
 	half_moves = stoi(half_move_c);
+
+	// move count
 
 	focus += 1;
 	string move_c;
@@ -363,23 +394,19 @@ void pos::parse_fen(string fen)
 	key = hashing::to_key(*this);
 }
 
-void pos::rook_moved(uint64 &sq)
+void pos::rook_moved(uint64 &sq64, uint16 sq)
 {
-	if (sq & pieces[ROOKS])
+	if (sq64 & pieces[ROOKS])
 	{
-		if (sq & side[WHITE])
+		if (sq64 & side[WHITE])
 		{
-			if (sq == 0x01)
-				castl_rights &= ~castl_right[SW];
-			else if (sq == 0x80)
-				castl_rights &= ~castl_right[LW];
+			if (sq == H1) castl_rights &= ~castl_right[SW];
+			else if (sq == A1) castl_rights &= ~castl_right[LW];
 		}
-		else if (sq & side[BLACK])
+		else
 		{
-			if (sq == 0x0100000000000000)
-				castl_rights &= ~castl_right[SB];
-			else if (sq == 0x8000000000000000)
-				castl_rights &= ~castl_right[LB];
+			if (sq == H8) castl_rights &= ~castl_right[SB];
+			else if (sq == A8) castl_rights &= ~castl_right[LB];
 		}
 	}
 }
