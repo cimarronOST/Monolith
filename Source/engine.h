007b081b50469dd18115d3b451460893e69ccebe
cimarronOST <cimarronOST@gmail.com>
2017-05-25 23:27:29 +0200

    Update engine.h

--- Source/engine.h
+++ Source/engine.h
@@ -1,5 +1,5 @@
 /*
-  Monolith 0.1  Copyright (C) 2017 Jonas Mayr
+  Monolith 0.2  Copyright (C) 2017 Jonas Mayr
 
   This file is part of Monolith.
 
@@ -27,40 +27,49 @@
 class engine
 {
 public:
-	//// position
+	// position
+
 	static void new_game(pos &board, chronos &chrono);
 	static void new_move(pos &board, uint64 &sq1, uint64 &sq2, uint8 flag);
 	static void parse_fen(pos &board, chronos &chrono, string fen);
 
-	//// bitboard
+	// bitboard
+
 	static uint32 bitscan(uint64 board);
 
-	//// movegen
+	// movegen
+
 	static void init_movegen();
 
-	//// book
-	static bool play_with_book;
+	// book
+
+	static bool use_book;
 	static void init_book();
 	static uint16 get_book_move(pos &board);
 
-	//// files
+	// files
+
 	static void init_path(string path);
 
-	//// random
+	// random
+
 	static void init_rand();
 
-	//// thread
+	// thread
+
 	static bool stop;
 
-	//// hash
-	static void init_hash(int size);
-	static void delete_hash();
+	// hash
+
+	static void new_hash_size(int size);
 	static int hash_size;
 
-	//// search
+	// search
+
 	static uint16 alphabeta(pos &board, chronos &chrono);
 	static int depth;
 
-	//// eval
+	// eval
+
 	static void init_eval();
 };
