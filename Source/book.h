4da06f31c58e99836b6bdf233e473f3558454f2c
cimarronOST <cimarronOST@gmail.com>
2017-05-25 23:24:13 +0200

    Update book.h

--- Source/book.h
+++ Source/book.h
@@ -1,12 +1,12 @@
 /*
-  Monolith 0.1  Copyright (C) 2017 Jonas Mayr
+  Monolith 0.2  Copyright (C) 2017 Jonas Mayr
 
   This file is part of Monolith.
 
   Monolith is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+  (at your option) any later version.
 
   Monolith is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,20 +23,20 @@
 #include "position.h"
 #include "main.h"
 
-struct book_entry
-{
-	uint64 key;
-	uint16 move;
-	uint16 count;
-	uint16 n;
-	uint16 sum;
-};
-
 namespace book
 {
+	struct book_entry
+	{
+		uint64 key;
+		uint16 move;
+		uint16 count;
+		uint16 n;
+		uint16 sum;
+	};
+
 	bool open();
 	uint16 get_move(pos &board);
 
-	int find_key(uint64 &key);
+	int find_key(const uint64 &key);
 	void read_entry(book_entry &entry, int index);
 };
