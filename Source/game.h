4e908413272cfc1b7d28dd865519864121f4c935
cimarronOST <cimarronOST@gmail.com>
2017-05-25 23:31:31 +0200

    Update game.h

--- Source/game.h
+++ Source/game.h
@@ -1,20 +1,20 @@
 /*
-  Monolith 0.1  Copyright(C) 2017 Jonas Mayr
+  Monolith 0.2  Copyright (C) 2017 Jonas Mayr
 
   This file is part of Monolith.
 
-  Monolith is free software : you can redistribute it and/or modify
+  Monolith is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
   Monolith is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Monolith.If not, see <http://www.gnu.org/licenses/>.
+  along with Monolith. If not, see <http://www.gnu.org/licenses/>.
 */
 
 
@@ -27,17 +27,16 @@ class game
 {
 public:
 	static int moves;
-	static string game_str;
 	static uint16 movelist[];
 	static uint64 hashlist[];
 
 	static void reset();
-	static void save_move(pos &board, uint16 move);
+	static void save_move(const pos &board, uint16 move);
 };
 
 namespace draw
 {
-	bool verify(pos &board, uint64 list[]);
-	bool by_material(pos &board);
-	bool by_rep(pos &board, uint64 list[], int size);
+	bool verify(const pos &board, uint64 list[], int depth);
+	bool by_rep(const pos &board, uint64 list[], int depth);
+	bool by_material(const pos &board);
 }
